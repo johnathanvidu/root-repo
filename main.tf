9eb4c50bc06ed914a82ee81434f224e02a696bbf
johnathanvidu <johnathan.v@qualisystems.com>
2022-09-19 00:25:30 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# authenticating via github token (token is configured as sensitive in the tf files!!!)
+provider "github" {
+  token = var.token
+}
+
+# this creates a new branch in a given repo
+resource "github_branch" "development" {
+  repository = var.repo
+  branch     = var.branch
+}
